d508de60e943a697563d9b41c5c63f6d1ece2436
Hanna Kulai <annakulay10@gmail.com>
2024-06-06 22:51:12 +0300

    add open ai dalle connector

---
+++ config.js
@@ -0,0 +1,44 @@
+export const config = {
+  connectorName: 'OpenAIDallE',
+  connectorVersion: '1.0.0',
+  models: ['dall-e-3', 'dall-e-2'],
+  description:
+    "The DALL-E Connector enables generating images from textual descriptions using OpenAI's DALL-E model.",
+  author: 'Prompt Mixer',
+  properties: [
+    {
+      id: 'n',
+      name: 'n',
+      value: 1,
+      type: 'number',
+    },
+    {
+      id: 'size',
+      name: 'size',
+      value: '1024x1024',
+      type: 'string',
+    },
+    {
+      id: 'style',
+      name: 'style',
+      value: 'vivid',
+      type: 'string',
+    },
+    {
+      id: 'quality',
+      name: 'quality',
+      value: 'hd',
+      type: 'string',
+    },
+  ],
+  settings: [
+    {
+      id: 'API_KEY',
+      name: 'API Key',
+      value: '',
+      type: 'string',
+    },
+  ],
+  iconBase64:
+    'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAILSURBVHgB1ZPNceJAEIX5O8BNIUAEy0aAHIHZCCwisLgBF8yFv4tFBJYzsCNARLA4grUjMCcKqAL2e1s9WzLCrvLNVtXUTP/odb83PbncV//y7wU6nU41n8/fsvzj8ejhWu73+8F0On3IfRbQwOYAPWJGm81mValUAs63+BLlsD+RE43H4+cPAcMw9Pj5juTVcDhsWQEf+47jC0CxYpybrAb2RRr0DWC32w1J7ouiEnEpUeBV7BY/JioYRZEAc71e7wZ/YzQaXWQA6SIwsJbRreEOWB5AodMUu260B+oM0NfD4fDTdVlwgIVC4ZpEJSXprklOAOsD9of4TJ2T+yJbfkur/sdxBxLrbG/A9BWLRXW1YP0SAwo00VZUa9g1m4DnDKAJXXU2I+JZoX/aodODGFDgEq3nhHx8Afs9/jADSGXRuXQgpVJJM9g47ZjYAn9b3ZodI8GVi5fcoVwuR9vt9hVd1NmA6rG7dV2I0RLIguIr/O5X+b0MIMPribYEp6oEl073hJZ26zP2mWmd/nzlZCjToTT01ut15AQXkG6fXWMR4v+RRtLAE9frmTnf6WDPobOcTCZt2W6I0y+FPTTtfouRLgp5orOAqTecAPxIdWlzZS9FMxpbXp9YHTYt92rOAjpQkgMAfHM1GKH2breLkUU6X+Nrnr7hdwHPFBDdGwEbxSdWcA7se3x/AeZhVUbFkV5NAAAAAElFTkSuQmCC',
+};
---
+++ main.ts
@@ -0,0 +1,107 @@
+import OpenAI from 'openai';
+import { config } from './config.js';
+import { ImageGenerateParams } from 'openai/resources/index.js';
+
+const API_KEY = 'API_KEY';
+
+interface ConnectorResponse {
+  Completions: { Content: string | ErrorCompletion; TokenUsage: undefined }[];
+  ModelType: string;
+}
+
+interface ErrorCompletion {
+  choices: Array<{
+    message: {
+      content: string;
+    };
+  }>;
+  error: string;
+  model: string;
+  usage: undefined;
+}
+
+const mapToResponse = (
+  outputs: Array<string[] | ErrorCompletion>,
+  model: string,
+): ConnectorResponse => {
+  return {
+    Completions: outputs.map((output) => {
+      if (Array.isArray(output)) {
+        return {
+          Content: output.join('\n\n'),
+          TokenUsage: undefined,
+        };
+      }
+      return {
+        Content: output,
+        TokenUsage: undefined,
+        Error: output.error,
+      };
+    }),
+    ModelType: model,
+  };
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const mapErrorToCompletion = (error: any, model: string): ErrorCompletion => {
+  const errorMessage = error.message || JSON.stringify(error);
+  return {
+    choices: [],
+    error: errorMessage,
+    model,
+    usage: undefined,
+  };
+};
+
+async function main(
+  model: string,
+  prompts: string[],
+  properties: Record<string, unknown>,
+  settings: Record<string, unknown>,
+) {
+  const { size, style, n, quality, response_format } =
+    properties as unknown as ImageGenerateParams;
+  const openai = new OpenAI({
+    apiKey: settings?.[API_KEY] as string,
+  });
+
+  const total = prompts.length;
+  const outputs: Array<string[] | ErrorCompletion> = [];
+
+  try {
+    for (let index = 0; index < total; index++) {
+      try {
+        const userPrompt = prompts[index];
+
+        const imageResponse: OpenAI.Images.ImagesResponse =
+          await openai.images.generate({
+            model,
+            prompt: userPrompt,
+            n,
+            size,
+            style,
+            quality,
+            response_format,
+          });
+
+        const responseToMarkdown = imageResponse.data.map(
+          (res) => `![${res.revised_prompt}](${res.url})`,
+        );
+
+        console.log(responseToMarkdown);
+
+        outputs.push(responseToMarkdown);
+      } catch (error) {
+        const completionWithError = mapErrorToCompletion(error, model);
+        outputs.push(completionWithError);
+      }
+    }
+
+    return mapToResponse(outputs, model);
+  } catch (error) {
+    console.error('Error in main function:', error);
+    return { Error: error, ModelType: model };
+  }
+}
+
+export { main, config };
